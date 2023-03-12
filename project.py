b3825a92556412a36d23db42f25e3380e7a47040
jasonk <jasonkhalili@gmail.com>
2023-03-12 15:11:13 -0700

    Password matching now works

--- project.py
+++ project.py
@@ -1,3 +1,7 @@
+# Talha Harooni - MySQL Server
+# Yasin Zahir - Python Frontend
+# Jason Khalili - Python Backend
+
 import mysql.connector
 import tkinter as tk
 from mysql.connector import Error
@@ -6,6 +10,7 @@ from tkinter import *
 def register():
     user = regUsername.get()
     passw = regpassword.get()
+    passwsecond = regpasswordsecond.get()
     firstname = regfirstname.get()
     lastname = reglastname.get()
     email = regemail.get()
@@ -20,28 +25,31 @@ def register():
 
     if userblank != 0:
         regexistsrow = tk.Label(root, text = "Username already exists             ")
-        regexistsrow.place(x = 150, y = 330)
+        regexistsrow.place(x = 150, y = 360)
     elif emailblank != 0:
         regexistsrow = tk.Label(root, text = "Email already exists             ")
-        regexistsrow.place(x = 150, y = 330)
+        regexistsrow.place(x = 150, y = 360)
     elif user == "":
         regexistsrow = tk.Label(root, text = "Username field is blank                 ")
-        regexistsrow.place(x = 150, y = 330)
+        regexistsrow.place(x = 150, y = 360)
     elif passw == "":
         regexistsrow = tk.Label(root, text = "Password field is blank                 ")
-        regexistsrow.place(x = 150, y = 330)
+        regexistsrow.place(x = 150, y = 360)
     elif firstname == "":
         regexistsrow = tk.Label(root, text = "First Name field is blank                 ")
-        regexistsrow.place(x = 150, y = 330)
+        regexistsrow.place(x = 150, y = 360)
     elif lastname == "":
         regexistsrow = tk.Label(root, text = "Last Name field is blank                 ")
-        regexistsrow.place(x = 150, y = 330)
+        regexistsrow.place(x = 150, y = 360)
     elif email == "":
         regexistsrow = tk.Label(root, text = "Email field is blank                        ")
-        regexistsrow.place(x = 150, y = 330)
+        regexistsrow.place(x = 150, y = 360)
+    elif passw != passwsecond:
+        regexistsrow = tk.Label(root, text = "Passwords do not match                        ")
+        regexistsrow.place(x = 150, y = 360)
     else:
         regexistsrow = tk.Label(root, text = "Registration Complete                                                                              ")
-        regexistsrow.place(x = 150, y = 330)
+        regexistsrow.place(x = 150, y = 360)
         cursor.execute("INSERT INTO User (username, password, firstname, lastname, email) VALUES (%s, %s, %s, %s, %s)", (user, passw, firstname, lastname, email))
         dataBase.commit()
 
@@ -120,31 +128,37 @@ regsecondrow.place(x = 50, y = 180)
 regpassword = tk.Entry(root, width = 35)
 regpassword.place(x = 150, y = 180, width = 100)
 
+regsecondrow = tk.Label(root, text = "Password: ")
+regsecondrow.place(x = 50, y = 210)
+
+regpasswordsecond = tk.Entry(root, width = 35)
+regpasswordsecond.place(x = 150, y = 210, width = 100)
+
 regthirdrow = tk.Label(root, text = "First Name: ")
-regthirdrow.place(x = 50, y = 210)
+regthirdrow.place(x = 50, y = 240)
 
 regfirstname = tk.Entry(root, width = 35)
-regfirstname.place(x = 150, y = 210, width = 100)
+regfirstname.place(x = 150, y = 240, width = 100)
 
 regfourthrow = tk.Label(root, text = "Last Name: ")
-regfourthrow.place(x = 50, y = 240)
+regfourthrow.place(x = 50, y = 270)
 
 reglastname = tk.Entry(root, width = 35)
-reglastname.place(x = 150, y = 240, width = 100)
+reglastname.place(x = 150, y = 270, width = 100)
 
 regfifthrow = tk.Label(root, text = "Email: ")
-regfifthrow.place(x = 50, y = 270)
+regfifthrow.place(x = 50, y = 300)
 
 regemail = tk.Entry(root, width = 35)
-regemail.place(x = 150, y = 270, width = 100)
+regemail.place(x = 150, y = 300, width = 100)
 
 registerbtn = tk.Button(root, text = "Register",
                         bg = 'blue', fg='white', command = register)
-registerbtn.place(x = 150, y = 300, width = 55)
+registerbtn.place(x = 150, y = 330, width = 55)
 
 resetbutton = tk.Button(root, text = "Reset Database",
                         bg = 'blue', fg='white', command = reset)
-resetbutton.place(x = 100, y = 360, width = 155)
+resetbutton.place(x = 100, y = 390, width = 155)
 
  
 root.mainloop()
